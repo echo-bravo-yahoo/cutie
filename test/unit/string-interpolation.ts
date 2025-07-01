0ddcc4fc474edaad5f7b8a90d19738c8f7395971
Ashton Eby <ashton.eby@gmail.com>
2025-07-01 14:44:24 -0700

    (refactor) move tests to TS

--- test/unit/string-interpolation.ts
+++ test/unit/string-interpolation.ts
@@ -1,7 +1,8 @@
 import { expect } from "chai";
 
-import MQTT from "../../src/outputs/mqtt.js";
+import MQTT, { MQTTConfig } from "../../src/outputs/mqtt.js";
 import { setGlobals } from "../../src/index.js";
+import Task from "../../src/util/generic-task.js";
 
 describe("string interpolation", function () {
   before(() => {
@@ -9,21 +10,24 @@ describe("string interpolation", function () {
   });
 
   it("works for nested module data", async function () {
-    const module = new MQTT({
-      device: { location: { shortName: "livingRoom" } },
-    });
+    const module = new MQTT(
+      {
+        device: { location: { shortName: "livingRoom" } },
+      } as unknown as MQTTConfig,
+      {} as Task
+    );
 
     const interpolated = module.interpolateConfigString(
-      "devices/${module.device.location.shortName}",
+      "devices/${module.device.location.shortName}"
     );
     expect(interpolated).to.deep.equal("devices/livingRoom");
   });
 
   it("works for nested global data", async function () {
-    const module = new MQTT({});
+    const module = new MQTT({} as MQTTConfig, {} as Task);
 
     const interpolated = module.interpolateConfigString(
-      "devices/${globals.deeply.nested}",
+      "devices/${globals.deeply.nested}"
     );
     expect(interpolated).to.deep.equal("devices/metadata");
   });
