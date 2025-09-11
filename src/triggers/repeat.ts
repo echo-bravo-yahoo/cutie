14b9526a4fd38d05c41b7fa5515636be5b4b080e
Ashton Eby <ashton.eby@gmail.com>
2025-09-10 20:07:12 -0700

    refactor: rename "trigger:interval" to "trigger:repeat"

--- src/triggers/repeat.ts
+++ src/triggers/repeat.ts
@@ -2,41 +2,41 @@ import Trigger, { TriggerConfig } from "../util/Trigger.js";
 import Task from "../util/Task.js";
 import { Message } from "../util/type-helpers.js";
 
-export interface IntervalConfig extends TriggerConfig {
+export interface RepeatConfig extends TriggerConfig {
   interval: number;
   message: Message;
 }
 
-export default class Interval extends Trigger {
-  declare config: IntervalConfig;
-  // @ts-expect-error interval is instantiated by enable()
-  interval: NodeJS.Timeout;
+export default class Repeat extends Trigger {
+  declare config: RepeatConfig;
+  // @ts-expect-error repeat is instantiated by enable()
+  repeat: NodeJS.Timeout;
 
-  constructor(config: IntervalConfig, task: Task) {
+  constructor(config: RepeatConfig, task: Task) {
     super(config, task);
   }
 
   async register() {}
 
   async enable() {
-    this.interval = setInterval(
+    this.repeat = setInterval(
       this.startMessage.bind(this, this.config.message),
       this.config.interval,
     );
-    this.info("Enabled interval.", { topic: this.logPrefix });
+    this.info("Enabled repeat.", { topic: this.logPrefix });
     this.enabled = true;
   }
 
   async disable() {
-    clearInterval(this.interval);
-    this.info("Disabled interval.", { topic: this.logPrefix });
+    clearInterval(this.repeat);
+    this.info("Disabled repeat.", { topic: this.logPrefix });
     this.enabled = false;
   }
 }
 
 /*
 {
-  "type": "interval",
+  "type": "repeat",
   "disabled": false,
   "message": { ... },
   "interval": 10000 // in ms
