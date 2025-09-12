ef7e6410e4237ac9d74b39d44750a3b89bb7791a
Ashton Eby <ashton.eby@gmail.com>
2025-09-12 13:46:47 -0700

    feat: update 'output:file' to do path interpolation

--- src/outputs/file.ts
+++ src/outputs/file.ts
@@ -1,4 +1,5 @@
 import { writeFile, appendFile } from "node:fs/promises";
+import { isAbsolute, resolve } from "node:path";
 
 import Output, { OutputConfig } from "../util/Output.js";
 import Task from "../util/Task.js";
@@ -29,10 +30,14 @@ export default class File extends Output {
     if (typeof message !== "string") message = JSON.stringify(message);
     if (this.config.insertNewlines === true) message = `\n${message}`;
 
+    let path = this.interpolateConfigString(this.config.path, { message });
+    if (!isAbsolute(path)) path = resolve(".", path);
+
     if (this.config.append) {
-      await appendFile(this.config.path, message);
+      // TO-DO: customize encoding
+      await appendFile(path, message, { encoding: "utf8" });
     } else {
-      await writeFile(this.config.path, message);
+      await writeFile(path, message, { encoding: "utf8" });
     }
 
     return message;
