9de1d840e69dcb554b3827dcee3bce1d719ce101
Ashton Eby <ashton.eby@gmail.com>
2025-09-26 10:52:19 -0700

    wip upload/download cli utilities

--- src/cli-entrypoint.ts
+++ src/cli-entrypoint.ts
@@ -2,9 +2,11 @@
 
 import { start } from "./index.js";
 import parser, { Arguments } from "yargs-parser";
-import initializeConfig from "./util/init.js";
+import initializeConfig from "./cli/init.js";
+import upload from "./cli/upload.js";
+import download from "./cli/download.js";
 
-export interface CLIArgs {
+export interface CLIArgs extends Arguments {
   config: string;
 }
 
@@ -13,10 +15,20 @@ const argv = parser(process.argv.slice(2) || "", {
   default: {
     config: `${process.cwd()}/cutie.conf.json`,
   },
-}) as Arguments & CLIArgs;
+}) as CLIArgs;
 
-if (argv._.length && argv._[0] === "init") {
+const subcommand = argv._.length ? argv._[0] : undefined;
+
+if (subcommand === "init") {
   initializeConfig(argv);
-} else {
+} else if (subcommand === "upload") {
+  upload(argv);
+} else if (subcommand === "download") {
+  download(argv);
+} else if (subcommand === "start") {
   start(argv);
+} else {
+  throw new Error(
+    `Unknown subcommand ${subcommand}! Valid subcommands are "start", "init", "upload", or "download".`,
+  );
 }
